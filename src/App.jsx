f946a2c22fbdbb35f9bff3a1aa94919fc48567ff
noel_vi <krichenyassine22@gmail.com>
2024-01-16 17:12:54 +0100

    last touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import TaskList from "./components/TaskList";
 
-function App() {
+const App = () => {
     return <TaskList />;
-}
+};
 
 export default App;
